f37aa9ade0e81ef26410b4af9ef22e00527069f7
aahilalwani25 <alwani.aahil25@gmail.com>
2025-07-09 12:45:42 +0500

    chore: made fields optional in send-email api

--- src/service/email.service.js
+++ src/service/email.service.js
@@ -7,16 +7,28 @@ const sendEmailService = async (body) => {
             to: `syedzubairalam123@gmail.com`,
             subject: `A client has approached to Co-VenTech`,
             html: `<h1>A client has approached to Co-VenTech</h1>
-    
-            <h2>Name</h2>
-            <p>${body.client_name}</h2>
+            ${body.client_name ? (
 
-            <h2>Email</h2>
-            <p>${body.from}</h2>
-    
-            <h2>About Company</h2>
-            <p>${body.company_name}</p>
-    
+                    `<h2>Name</h2>
+                    <p>${body.client_name}</h2>
+`                ) : ''
+                }
+
+            ${body.from ? (
+
+                    `<h2>Email</h2>
+                    <p>${body.from}</h2>
+    `
+                ) : ''
+                }
+
+            ${body.company_name ? (
+
+                    `         <h2>About Company</h2>
+                    <p>${body.company_name}</p>
+    `
+                ) : ''
+                }
             <h2>Interested Service</h2>
             <p>${body.interested_service}</p>
                    `,
@@ -35,4 +47,4 @@ const sendEmailService = async (body) => {
     }
 }
 
-module.exports={sendEmailService}
+module.exports = { sendEmailService }
--- src/validators/email.validator.js
+++ src/validators/email.validator.js
@@ -1,11 +1,11 @@
 const Joi = require("joi");
 
 const emailValidation= Joi.object({
-    from: Joi.string().email(),
+    from: Joi.string().email().optional(),
     to: Joi.string().email(),
     interested_service: Joi.string(),
-    company_name: Joi.string(),
-    client_name: Joi.string()
+    company_name: Joi.string().optional(),
+    client_name: Joi.string().optional()
 });
 
 module.exports={emailValidation};
